2cf1a6aeb9c027531b215a4d32812c14e0c4bd8d
DavFranco <davinitafranco@gmail.com>
2022-05-12 09:19:08 -0300

    Se instala  Node

---
+++ ProyectoAngular/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './Componentes/header/header.component';
+import { LogoAPComponent } from './Componentes/logo-ap/logo-ap.component';
+import { BannerComponent } from './Componentes/banner/banner.component';
+import { SocialComponent } from './Componentes/social/social.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    BannerComponent,
+    SocialComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
